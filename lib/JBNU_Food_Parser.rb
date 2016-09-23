1f78a61a0728ccfede758f518b92c381339f3c50
hmu332233 <hmu332233@naver.com>
2016-09-23 08:33:09 +0000

    구조변경

--- lib/JBNU_Food_Parser.rb
+++ lib/JBNU_Food_Parser.rb
@@ -15,14 +15,13 @@ class JBNU_Food_Parser
         return doc
     end
     
-    
-    def requestJinsu
+    def requestMenu(place_id)
         
         doc = requestHTML
         
         tables = doc.css("#sub_right//table")
         
-        t = tables[0].css("tr//td")
+        t = tables[place_id].css("tr//td")
         
         rt = []
         t.each_with_index do |a,i|
@@ -30,81 +29,30 @@ class JBNU_Food_Parser
             rt << a.inner_text.to_s.squish
         end
         
+        if place_id == 0
+            place = "진수원(진수당)"
+        elsif place_id == 1
+            place = "의대"
+        elsif place_id == 2
+            place == "학생회관"
+        end
+        
         menus = [
-            Menu.new("진수원","월","중식","백반",[rt[9],rt[15],rt[20],rt[25]]),
-            Menu.new("진수원","화","중식","백반",[rt[10],rt[16],rt[21],rt[26]]),
-            Menu.new("진수원","수","중식","백반",[rt[11],rt[17],rt[22],rt[27]]),
-            Menu.new("진수원","목","중식","백반",[rt[12],rt[18],rt[23],rt[28]]),
-            Menu.new("진수원","금","중식","백반",[rt[13],rt[19],rt[24],rt[29]]),
-            Menu.new("진수원","월","석식","백반",[rt[32],rt[37],rt[42],rt[47]]),
-            Menu.new("진수원","화","석식","백반",[rt[33],rt[38],rt[43],rt[48]]),
-            Menu.new("진수원","수","석식","백반",[rt[34],rt[39],rt[44],rt[49]]),
-            Menu.new("진수원","목","석식","백반",[rt[35],rt[40],rt[45],rt[50]]),
-            Menu.new("진수원","금","석식","백반",[rt[36],rt[41],rt[46],rt[51]])
+            Menu.new(place,"월","중식","백반",[rt[9],rt[15],rt[20],rt[25]]),
+            Menu.new(place,"화","중식","백반",[rt[10],rt[16],rt[21],rt[26]]),
+            Menu.new(place,"수","중식","백반",[rt[11],rt[17],rt[22],rt[27]]),
+            Menu.new(place,"목","중식","백반",[rt[12],rt[18],rt[23],rt[28]]),
+            Menu.new(place,"금","중식","백반",[rt[13],rt[19],rt[24],rt[29]]),
+            Menu.new(place,"월","석식","백반",[rt[32],rt[37],rt[42],rt[47]]),
+            Menu.new(place,"화","석식","백반",[rt[33],rt[38],rt[43],rt[48]]),
+            Menu.new(place,"수","석식","백반",[rt[34],rt[39],rt[44],rt[49]]),
+            Menu.new(place,"목","석식","백반",[rt[35],rt[40],rt[45],rt[50]]),
+            Menu.new(place,"금","석식","백반",[rt[36],rt[41],rt[46],rt[51]])
             ]
         
         
         return menus
     end
     
-    def requestMediMenu
-        
-        doc = requestHTML
-        
-        tables = doc.css("#sub_right//table")
-        
-        t = tables[1].css("tr//td")
-        
-        rt = []
-        t.each_with_index do |a,i|
-            print i.to_s + " : " +a.inner_text + "\n"
-            rt << a.inner_text.to_s.squish
-        end
-        
-        menus = [
-            Menu.new("의대","월","중식","백반",[rt[9],rt[15],rt[20],rt[25]]),
-            Menu.new("의대","화","중식","백반",[rt[10],rt[16],rt[21],rt[26]]),
-            Menu.new("의대","수","중식","백반",[rt[11],rt[17],rt[22],rt[27]]),
-            Menu.new("의대","목","중식","백반",[rt[12],rt[18],rt[23],rt[28]]),
-            Menu.new("의대","금","중식","백반",[rt[13],rt[19],rt[24],rt[29]]),
-            Menu.new("의대","월","석식","백반",[rt[32],rt[37],rt[42],rt[47]]),
-            Menu.new("의대","화","석식","백반",[rt[33],rt[38],rt[43],rt[48]]),
-            Menu.new("의대","수","석식","백반",[rt[34],rt[39],rt[44],rt[49]]),
-            Menu.new("의대","목","석식","백반",[rt[35],rt[40],rt[45],rt[50]]),
-            Menu.new("의대","금","석식","백반",[rt[36],rt[41],rt[46],rt[51]])
-            ]
-        
-        return menus
-    end
-    
-    def requestStudentHallMenu
-        
-        doc = requestHTML
-        
-        tables = doc.css("#sub_right//table")
-        
-        t = tables[2].css("tr//td")
-        
-        rt = []
-        t.each_with_index do |a,i|
-            print i.to_s + " : " +a.inner_text + "\n"
-            rt << a.inner_text.to_s.squish
-        end
-        
-        menus = [
-            Menu.new("의대","월","조식/중식","백반",[rt[9],rt[15],rt[21],rt[27]]),
-            Menu.new("의대","화","조식/중식","백반",[rt[10],rt[16],rt[22],rt[28]]),
-            Menu.new("의대","수","조식/중식","백반",[rt[11],rt[17],rt[23],rt[29]]),
-            Menu.new("의대","목","조식/중식","백반",[rt[12],rt[18],rt[24],rt[30]]),
-            Menu.new("의대","금","조식/중식","백반",[rt[13],rt[19],rt[25],rt[31]]),
-            Menu.new("의대","월","석식","백반",[rt[35],rt[41],rt[47],rt[53]]),
-            Menu.new("의대","화","석식","백반",[rt[36],rt[42],rt[48],rt[54]]),
-            Menu.new("의대","수","석식","백반",[rt[37],rt[43],rt[49],rt[55]]),
-            Menu.new("의대","목","석식","백반",[rt[38],rt[44],rt[50],rt[56]]),
-            Menu.new("의대","금","석식","백반",[rt[39],rt[45],rt[51],rt[57]])
-            ]
-        
-        return menus
-    end
 
 end
--- lib/Message_Manager.rb
+++ lib/Message_Manager.rb
@@ -23,53 +23,23 @@ class Message_Manager
     
     
     def getJinsuMenu
-        
-        parser = JBNU_Food_Parser.new
-        menus = parser.requestJinsu
-        
-        head_text = "진수원(진수당)" + "\n\n\n"
-        contents = ""
-        
-        menus.each do |menu|
-            text = menu.week + "(" + menu.time + ") - " + menu.category+"\n\n"
-            menu.contents.each do |m|
-                text += m + "\n"
-            end
-            
-            contents += text + "\n\n"
-        end
-        
-        return head_text + contents.to_s.chop!.chop!.chop!
-        
+        return makeMenuText(0)
     end
     
     def getMediMenu
-        
-        parser = JBNU_Food_Parser.new
-        menus = parser.requestMediMenu
-        
-        head_text = "의대" + "\n\n\n"
-        contents = ""
-        
-        menus.each do |menu|
-            text = menu.week + "(" + menu.time + ") - " + menu.category+"\n\n"
-            menu.contents.each do |m|
-                text += m + "\n"
-            end
-            
-            contents += text + "\n\n"
-        end
-        
-        return head_text + contents.to_s.chop!.chop!.chop!
-        
+        return makeMenuText(1)
     end
     
     def getStudentHallMenu
+        return makeMenuText(2)
+    end
+
+    def makeMenuText(id)
         
         parser = JBNU_Food_Parser.new
-        menus = parser.requestStudentHallMenu
+        menus = parser.requestMenu(id)
         
-        head_text = "학생회관" + "\n\n\n"
+        # head_text = menus[0].shop_name + "\n\n\n"
         contents = ""
         
         menus.each do |menu|
@@ -81,8 +51,7 @@ class Message_Manager
             contents += text + "\n\n"
         end
         
-        return head_text + contents.to_s.chop!.chop!.chop!
+        return contents.to_s.chop!.chop!.chop!
     end
-
     
 end
