8b41a0715240ac47d06309a65b066846ef80cc2e
hmu332233 <hmu332233@naver.com>
2016-09-23 21:00:59 +0000

    날짜 테스트

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -12,29 +12,32 @@ class KakaoController < ApplicationController
     end
     
     def message
+        
+        day = Time.now.strftime("%A").to_s
      
         result="잘못된 명령어입니다. \n '알려줘 도움말' 을 입력하시면 자세한 사용방법을 알려드립니다"
         
         content = params[:content].split
         message_Manager = Message_Manager.new
         
-        # if content[0] == "알려줘"
-        #     if content[1] == "학사공지"
-        #         result = message_Manager.getMessage_Notice(0)
-        #     elsif content[1] == "알바"
-        #         result = message_Manager.getMessage_Notice(5)
-        #     elsif content[1] == "진수당" || content[1] == "진수원"
-        #         result = message_Manager.getJinsuMenu
-        #     elsif content[1] == "의대"
-        #         result = message_Manager.getMediMenu
-        #     elsif content[1] == "학생회관"
-        #         result = message_Manager.getStudentHallMenu
-        #     end
-        # end
+        if content[0] == "알려줘"
+            if content[1] == "학사공지"
+                result = message_Manager.getMessage_Notice(0)
+            elsif content[1] == "알바"
+                result = message_Manager.getMessage_Notice(5)
+            elsif content[1] == "진수당" || content[1] == "진수원"
+                result = message_Manager.getJinsuMenu(dayNumber(day))
+            elsif content[1] == "의대"
+                result = message_Manager.getMediMenu(dayNumber(day))
+            elsif content[1] == "학생회관"
+                result = message_Manager.getStudentHallMenu(dayNumber(day))
+            end
+        end
         
+            
+     
         
-        result = Time.now.strftime("%A")
-        
+    
        
         render json: {
                 "message":{
@@ -54,6 +57,27 @@ class KakaoController < ApplicationController
     
     def chat_room
         render json: ""
+    end
+
+
+    def dayNumber(day)
+    
+        case day
+        when 'Monday'
+            result = 0
+        when 'Tuesday'
+            result = 1
+        when 'Wednesday'
+            result = 2
+        when 'Thursday'
+            result = 3
+        when 'Friday'
+            result = 4
+        else
+           result = 5
+        end
+        
+        return result
     end
     
 end
--- lib/Message_Manager.rb
+++ lib/Message_Manager.rb
@@ -23,18 +23,32 @@ class Message_Manager
     
     
     def getJinsuMenu
-        return makeMenuText(0)
+        return makeMenuTextAll(0)
+    end
+    
+    def getJinsuMenu(day)
+        return makeMenuTextDay(0,day)
     end
     
     def getMediMenu
-        return makeMenuText(1)
+        return makeMenuTextAll(1)
+    end
+    
+    def getMediMenu(day)
+        return makeMenuTextDay(1,day)
     end
     
     def getStudentHallMenu
-        return makeMenuText(2)
+        return makeMenuTextAll(2)
     end
+    
+    def getStudentHallMenu(day)
+        return makeMenuTextDay(2,day)
+    end
+    
+    
 
-    def makeMenuText(id)
+    def makeMenuTextAll(id)
         
         parser = JBNU_Food_Parser.new
         menus = parser.requestMenu(id)
@@ -43,6 +57,28 @@ class Message_Manager
         contents = ""
         
         menus.each do |menu|
+            text = menu.week + "(" + menu.time + ") - " + menu.category+"\n\n"
+            menu.contents.each do |m|
+                text += m + "\n"
+            end
+            
+            contents += text + "\n\n"
+        end
+        
+        return contents.to_s.chop!.chop!.chop!
+    end
+    
+    def makeMenuTextDay(id,day)
+        
+        parser = JBNU_Food_Parser.new
+        menus = parser.requestMenu(id)
+        
+        # head_text = menus[0].shop_name + "\n\n\n"
+        contents = ""
+        
+        menus_day = [menus[day],menus[day+5]]
+        
+        menus_day.each do |menu|
             text = menu.week + "(" + menu.time + ") - " + menu.category+"\n\n"
             menu.contents.each do |m|
                 text += m + "\n"
