35bfbc675aa528184b6d1257944311f5557713a3
minung han <hmu332233@gmail.com>
2018-02-06 02:29:10 +0900

    [Add] function testAll

--- app/assets/javascripts/tester.js
+++ app/assets/javascripts/tester.js
@@ -1,7 +1,31 @@
 tester = function () {
-    
+  
+  this.testcases = [
+    '참빛',
+    '새빛',
+    '후생관',
+    '진수당',
+    '정담원',
+    '학생회관',
+    '특성화',
+    '의대'
+  ];
 };
 
+
+tester.prototype.testAll = function (testcases, callback) {
+  var self = this;  
+  
+  var requests = testcases.map(function(testcase) {
+    return self.requestMessage(testcase);
+  });
+  
+  requests.forEach(function (request) {
+    request.done(function (response) {
+      callback(response.message.text);
+    });
+  });
+};
 
 /*
 * message: {string} 입력할 메세지 이름  
@@ -21,8 +45,5 @@ tester.prototype.requestMessage = function (message, successCallback) {
     "data": "{\"content\": \""+ message +"\"}"
   }
   
-  $.ajax(settings).done(function (response) {
-    console.log(response.message.text);
-  });
-    
+  return $.ajax(settings);
 };
