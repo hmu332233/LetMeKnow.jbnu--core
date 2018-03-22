a6c72b356e5426385fd7401c596cfb21e26bf831
minung han <hmu332233@gmail.com>
2018-03-23 01:40:01 +0900

    [Modify] 1 노선 버스 순환 메시지 출력

--- app/helpers/BusTracer.rb
+++ app/helpers/BusTracer.rb
@@ -2,7 +2,7 @@ require 'util/TimeHelper'
 
 class BusTracer
   
-  def getNameData(bus_no, index) 
+  def getNameData(bus_no, index = nil) 
     no1 = [
       '생활관(대동관)',
       '2학생회관 측면',
@@ -28,7 +28,13 @@ class BusTracer
       '생활관[대동관]'
     ]
     
-    
+    if index.nil?
+      if bus_no == 0
+        return no1
+      else
+        return no2
+      end
+    end
     
     if bus_no == 0
       
@@ -49,6 +55,35 @@ class BusTracer
       
       return no2[index]
     end
+  end
+  
+  def getTimeWithIndex(bus_no, course_index, arriving_index)
+    timeData = getTimeData('', bus_no)
+    
+    if arriving_index > timeData.size - 1
+      arriving_index = 0
+    elsif arriving_index < 0
+      arriving_index = timeData.size - 1
+    end
+    return timeData[arriving_index][course_index]
+  end
+  
+  def getCourseTimeData(bus_no, course_index)
+    timeData = getTimeData('reverse', bus_no)
+    return timeData[course_index]
+  end
+   
+  def convertArrivingIndex(bus_no, arriving_index)
+    
+    timeData = getTimeData('', bus_no)
+    
+    if arriving_index > timeData.size - 1
+      arriving_index = 0
+    elsif arriving_index < 0
+      arriving_index = timeData.size - 1
+    end
+    
+    return arriving_index
   end
    
   def getTimeData(type, index)
@@ -117,8 +152,8 @@ class BusTracer
      
     return {
       course_index: course_index,
-      past_stop: arriving_index-2,
-      current_stop: arriving_index-1,
+      past_stop: convertArrivingIndex(no, arriving_index-2),
+      current_stop: convertArrivingIndex(no, arriving_index-1),
       next_stop: arriving_index
     }
   end
@@ -144,7 +179,12 @@ class BusTracer
     splited_time1 = [now.hour, now.min]
     splited_time2 = target_time.split(':')
   
-    return (DateTime.now.change({hour: splited_time1[0], min: splited_time1[1]}) - DateTime.now.change({hour: splited_time2[0].to_i, min: splited_time2[1].to_i})).positive?
+    diff = (DateTime.now.change({hour: splited_time1[0], min: splited_time1[1]}) - DateTime.now.change({hour: splited_time2[0].to_i, min: splited_time2[1].to_i}))
+    if diff == 0 || diff.positive?
+      return true
+    else
+      return false
+    end
   end
   
 end
--- app/helpers/message/M_Bus.rb
+++ app/helpers/message/M_Bus.rb
@@ -12,7 +12,56 @@ module M_Bus
         current_stop = busTracer.getNameData(0, bus_location[:current_stop])
         next_stop = busTracer.getNameData(0, bus_location[:next_stop])
         
-        return "#{past_stop_name}\n#{current_stop}\n#{next_stop}"
+        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:past_stop])
+        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:current_stop])
+        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:next_stop])
+        
+        course_name_list = busTracer.getNameData(0)
+        course_time_list = busTracer.getCourseTimeData(0, bus_location[:course_index])
+        
+        now = TimeHelper.new.now
+        
+        text = "#{now}\n"
+        
+        course_name_list.each_with_index do |name, i|
+            if i == bus_location[:current_stop]
+                text += "#{name} <===\n#{course_time_list[i]}\n\n"
+            elsif i == bus_location[:past_stop] || i == bus_location[:next_stop]
+                text += "#{name} <=\n#{course_time_list[i]}\n\n"
+            else
+                text += "#{name}\n#{course_time_list[i]}\n\n"
+            end
+        end
+        
+        return "#{text}"
+    end
+    
+    def makeMessage_bus_location_with_index(bus_no)
+        busTracer = BusTracer.new
+        bus_location = busTracer.trace(bus_no)
+        
+        past_stop_name = busTracer.getNameData(bus_no, bus_location[:past_stop])
+        current_stop = busTracer.getNameData(bus_no, bus_location[:current_stop])
+        next_stop = busTracer.getNameData(bus_no, bus_location[:next_stop])
+
+        course_name_list = busTracer.getNameData(bus_no)
+        course_time_list = busTracer.getCourseTimeData(bus_no, bus_location[:course_index])
+        
+        now = TimeHelper.new.now
+        
+        text = "#{now}\n"
+        
+        course_name_list.each_with_index do |name, i|
+            if i == bus_location[:current_stop]
+                text += "#{name} <===\n#{course_time_list[i]}\n\n"
+            elsif i == bus_location[:past_stop] || i == bus_location[:next_stop]
+                text += "#{name} <=\n#{course_time_list[i]}\n\n"
+            else
+                text += "#{name}\n#{course_time_list[i]}\n\n"
+            end
+        end
+        
+        return "#{text}"
     end
     
     def makeMessage_bus_time
