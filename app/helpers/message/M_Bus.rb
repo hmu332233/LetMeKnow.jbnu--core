ca1776e0a25f7f5aa77a41a085c0faba45dd0bc1
minung han <hmu332233@gmail.com>
2018-03-23 02:13:51 +0900

    [Modify] 1노선 2노선 위치 표시하도록

--- app/helpers/message/M_Bus.rb
+++ app/helpers/message/M_Bus.rb
@@ -4,34 +4,15 @@ module M_Bus
     
     def makeMessage_bus_location
         
-        busTracer = BusTracer.new
+         now = TimeHelper.new.now
         
-        bus_location = busTracer.trace(0)
-        
-        past_stop_name = busTracer.getNameData(0, bus_location[:past_stop])
-        current_stop = busTracer.getNameData(0, bus_location[:current_stop])
-        next_stop = busTracer.getNameData(0, bus_location[:next_stop])
-        
-        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:past_stop])
-        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:current_stop])
-        puts busTracer.getTimeWithIndex(0, bus_location[:course_index], bus_location[:next_stop])
-        
-        course_name_list = busTracer.getNameData(0)
-        course_time_list = busTracer.getCourseTimeData(0, bus_location[:course_index])
-        
-        now = TimeHelper.new.now
-        
-        text = "#{now}\n"
-        
-        course_name_list.each_with_index do |name, i|
-            if i == bus_location[:current_stop]
-                text += "#{name} <===\n#{course_time_list[i]}\n\n"
-            elsif i == bus_location[:past_stop] || i == bus_location[:next_stop]
-                text += "#{name} <=\n#{course_time_list[i]}\n\n"
-            else
-                text += "#{name}\n#{course_time_list[i]}\n\n"
-            end
-        end
+        text = "#{now}\n\n"
+        text += "◆제 1노선 [순환노선]◆\n\n"
+        text += makeMessage_bus_location_with_index(0)
+        text += "\n"
+        text += "◆제 2노선 [왕복노선]◆\n\n"
+        text += makeMessage_bus_location_with_index(1)
+        text += "◎: 예상위치 ○: 오차범위"
         
         return "#{text}"
     end
@@ -46,18 +27,16 @@ module M_Bus
 
         course_name_list = busTracer.getNameData(bus_no)
         course_time_list = busTracer.getCourseTimeData(bus_no, bus_location[:course_index])
-        
-        now = TimeHelper.new.now
-        
-        text = "#{now}\n"
+ 
+        text = ""
         
         course_name_list.each_with_index do |name, i|
             if i == bus_location[:current_stop]
-                text += "#{name} <===\n#{course_time_list[i]}\n\n"
+                text += "◎ #{name}\n    #{course_time_list[i]}\n\n"
             elsif i == bus_location[:past_stop] || i == bus_location[:next_stop]
-                text += "#{name} <=\n#{course_time_list[i]}\n\n"
+                text += "○ #{name}\n    #{course_time_list[i]}\n\n"
             else
-                text += "#{name}\n#{course_time_list[i]}\n\n"
+                text += "    #{name}\n    #{course_time_list[i]}\n\n"
             end
         end
         
