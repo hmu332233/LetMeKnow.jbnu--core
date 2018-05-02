50ac5289af59a277c926577b0b3a2cc55f37753b
minung.han <hmu332233@gmail.com>
2018-05-03 00:51:48 +0900

    [Add] 도서관 좌석수 파싱 스켈레톤 작성

--- app/helpers/parser/LibraryParser.rb
+++ app/helpers/parser/LibraryParser.rb
@@ -9,7 +9,22 @@ class LibraryParser
   end
   
   def parseData(doc)
-    return '빈 자리 데이터'
+    
+    trs = doc.css("table.default//tr")
+    
+    trs.each_with_index do |tr|
+      tds = tr.css("td")
+      
+      puts tds.inner_text
+      if tds.length == 3
+        puts "#{tds[0].inner_text}"
+        puts "#{tds[1].inner_text}"
+        puts "#{tds[2].inner_text}\n"
+      end
+      
+    end
+
+    return '이건 자리'
   end
   
 end
