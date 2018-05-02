1bd8248a82e4168c55443b731ee1e66e7a815d9a
minung.han <hmu332233@gmail.com>
2018-05-03 01:12:35 +0900

    [Modify] LibraryParser 완성

--- app/helpers/parser/LibraryParser.rb
+++ app/helpers/parser/LibraryParser.rb
@@ -1,4 +1,5 @@
 require 'util/HttpRequester'
+require 'LibrarySeat'
 
 class LibraryParser
 
@@ -10,21 +11,27 @@ class LibraryParser
   
   def parseData(doc)
     
-    trs = doc.css("table.default//tr")
+    librarySeats = []
     
+    trs = doc.css("table.default//tr")
     trs.each_with_index do |tr|
+      unless tr.attribute('bgcolor').to_s == "#ffffff"
+        next
+      end
+      
       tds = tr.css("td")
       
-      puts tds.inner_text
       if tds.length == 3
-        puts "#{tds[0].inner_text}"
-        puts "#{tds[1].inner_text}"
-        puts "#{tds[2].inner_text}\n"
+        name = tds[0].inner_text.strip
+        total = tds[1].inner_text.strip
+        spare = tds[2].inner_text.strip
+     
+        librarySeats << LibrarySeat.new(name, total, spare)
       end
       
     end
 
-    return '이건 자리'
+    return librarySeats
   end
   
 end
