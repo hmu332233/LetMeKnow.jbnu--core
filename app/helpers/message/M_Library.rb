f9443422c3a53ff98edb02f82081def20cc6e83f
minung.han <hmu332233@gmail.com>
2018-05-04 00:25:28 +0900

    [Modify] 중도 자리 메세지 수정

--- app/helpers/message/M_Library.rb
+++ app/helpers/message/M_Library.rb
@@ -3,13 +3,13 @@ require 'parser/LibraryParser'
 module M_Library
 
   def makeMessage_library_empty_seat
-    
     parser = LibraryParser.new
-    data = parser.requestEmptySeatData
+    data = parser.requestEmptySeatData[0..17]
     
-    text = ''
-    text += data
-    
-    return "#{text}"
+    text = "◆ 중도 자리 ◆\n\n"
+    data.each do |librarySeat|
+      text += "#{librarySeat.name}\n사용/총 좌석: #{librarySeat.total}\n\n"
+    end
+    return "#{text.strip}"
   end
 end
