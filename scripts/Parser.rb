41eb391ab90c2c4eb81828b6554234f3178c624a
minung.han <hmu332233@gmail.com>
2018-08-02 00:38:23 +0900

    [Add] scripts dir

--- scripts/Parser.rb
+++ scripts/Parser.rb
@@ -1,16 +1,16 @@
+require 'net/http'
+require 'nokogiri'
+
 class Parser
     
-    def parser
-        
-        
+    def parse
         uri = URI(URI.encode("http://www.chonbuk.ac.kr/pj/pjDept.php?movePage=1&pageKey=63&TP=001&pid=336"))
        
         req = Net::HTTP::Get.new(uri)
     
         res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
         doc = Nokogiri::HTML(res.body)
-        
-        
+
         table = doc.css(".table01//tbody//tr")
         
         
@@ -26,12 +26,9 @@ class Parser
             m = name.inner_text.squish.strip + "/" + office.inner_text.squish.strip + "/" + tel.inner_text.squish.strip.split("Fax")[0]+"/"+"Fax"+tel.inner_text.squish.strip.split("Fax")[1]
             
             print st + m + en + ",\n"
-            
+
         end
-        
-        
-        
-        
-    end
-    
+    end   
 end
+
+Parser.new.parse
