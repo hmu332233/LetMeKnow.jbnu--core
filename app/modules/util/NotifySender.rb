cb6353ba7a20bcd235e1d5449fca2e91a256ea74
minung.han <hmu332233@gmail.com>
2018-08-05 00:15:41 +0900

    [Add] NotifySender module

---
+++ app/modules/util/NotifySender.rb
@@ -0,0 +1,25 @@
+require 'net/http'
+require 'uri'
+
+class NotifySender
+  def send(message)
+    @@TARGET_URL = 'https://notify-api.line.me/api/notify'
+    @@TOKEN = ENV['notify_token']
+
+    header = {
+      'Authorization': 'Bearer ' + @@TOKEN
+    }
+
+    uri = URI.parse(@@TARGET_URL)
+    https = Net::HTTP.new(uri.host,uri.port)
+    https.use_ssl = true
+    req = Net::HTTP::Post.new(uri.path, header)
+    req.set_form_data({'message' => message}, ';')
+    res = https.request(req)
+    
+    puts "Response #{res.code} #{res.message}: #{res.body}"
+  end
+end
+
+
+
