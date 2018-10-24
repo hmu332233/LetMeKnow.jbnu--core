05e0b5dc206cd55e51ddeb395f2126fd5565be57
minung.han <hmu332233@gmail.com>
2018-10-25 00:19:22 +0900

    [Add] managementApi 날리도록

--- app/modules/util/HttpRequester.rb
+++ app/modules/util/HttpRequester.rb
@@ -1,11 +1,18 @@
-require 'util/TimeHelper'
-
 class HttpRequester
   def self.requestNokogiriHtml(url)
     uri = URI(URI.encode(url))  
     req = Net::HTTP::Get.new(uri)
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
     doc = Nokogiri::HTML(res.body)
-    return doc    
+    return doc
+  end
+  
+  def self.requestPostWithJson(url, json)
+    uri = URI(url)
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = json
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
   end
 end
