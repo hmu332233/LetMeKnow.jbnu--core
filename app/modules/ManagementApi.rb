8e933c93f5dec678d4e81ff1932989a3144b59f4
minung.han <hmu332233@gmail.com>
2019-04-21 00:04:30 +0900

    [Add] 외부 menu 가져오는 api 추가

--- app/modules/ManagementApi.rb
+++ app/modules/ManagementApi.rb
@@ -4,7 +4,25 @@ class ManagementApi
   def self.sendUserWord(userKey, messageContent)
     @@TARGET_URL = ENV['management_server']
     @@JSON = { id: userKey, content: messageContent }.to_json
-    HttpRequester.requestPostWithJson(@@TARGET_URL, @@JSON)
+    HttpRequester.requestPostWithJson("#{@@TARGET_URL}/api/v1/user_words", @@JSON)
+  end
+  
+  def self.getJinsuMenu()
+    @@TARGET_URL = ENV['management_server']
+    json = HttpRequester.requestGetReturnJson("#{@@TARGET_URL}/api/v1/menus/jinsu")
+    return json
+  end
+  
+  def self.getMediMenu()
+    @@TARGET_URL = ENV['management_server']
+    json = HttpRequester.requestGetReturnJson("#{@@TARGET_URL}/api/v1/menus/medi")
+    return json
+  end
+  
+  def self.getStudentHallMenu()
+    @@TARGET_URL = ENV['management_server']
+    json = HttpRequester.requestGetReturnJson("#{@@TARGET_URL}/api/v1/menus/studentHall")
+    return json
   end
 end
 
