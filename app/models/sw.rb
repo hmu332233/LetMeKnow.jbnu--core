78f748bed0d35050d763d58ed11a90bc83a4debf
minung.han <hmu332233@gmail.com>
2019-05-21 02:23:01 +0900

    [Add] 각 식당별 db 스위칭용 속성 추가

--- app/models/sw.rb
+++ app/models/sw.rb
@@ -1,2 +1,69 @@
 class Sw < ActiveRecord::Base
+  JINSU = 'JINSU'
+  MEDI = 'MEDI'
+  HU = 'HU'
+  JUNGDAM = 'JUNGDAM'
+  JINSU = 'JINSU'
+  STUDENT_HALL = 'STUDENT_HALL'
+  
+  def self.useDbMenu?(type)    
+    case type
+      when MEDI
+        return useMediDbMenu?
+      when HU
+        return useHuDbMenu?
+      when JUNGDAM
+        return useJungdamDbMenu?
+      when JINSU
+        return useJinsuDbMenu?
+      when STUDENT_HALL
+        return useStudentHallDbMenu?
+    end
+  end
+  
+  def self.useMediDbMenu?
+    begin
+      useDbMenu = Sw.find(1).use_db_medi
+    rescue
+      useDbMenu = Sw.create.use_db_medi
+    end
+    return useDbMenu
+  end
+  
+  def self.useHuDbMenu?
+    begin
+      useDbMenu = Sw.find(1).use_db_hu
+    rescue
+      useDbMenu = Sw.create.use_db_hu
+    end
+    return useDbMenu
+  end
+  
+  def self.useJungdamDbMenu?
+    begin
+      useDbMenu = Sw.find(1).use_db_jungdam
+    rescue
+      useDbMenu = Sw.create.use_db_jungdam
+    end
+    return useDbMenu
+  end
+  
+  def self.useJinsuDbMenu?
+    begin
+      useDbMenu = Sw.find(1).use_db_jinsu
+    rescue
+      useDbMenu = Sw.create.use_db_jinsu
+    end
+    return useDbMenu
+  end
+  
+  def self.useStudentHallDbMenu?
+    begin
+      useDbMenu = Sw.find(1).use_db_student_hall
+    rescue
+      useDbMenu = Sw.create.use_db_student_hall
+    end
+    return useDbMenu
+  end
+    
 end
--- db/migrate/20170430124356_create_sws.rb
+++ db/migrate/20170430124356_create_sws.rb
@@ -6,6 +6,12 @@ class CreateSws < ActiveRecord::Migration
       t.boolean :use_db_menu, :default => true
       t.boolean :use_yesterday_domitory_menu, :default => false
       
+      t.boolean :use_db_medi, :default => true
+      t.boolean :use_db_hu, :default => true
+      t.boolean :use_db_jungdam, :default => true
+      t.boolean :use_db_jinsu, :default => true
+      t.boolean :use_db_student_hall, :default => true
+      
       t.timestamps null: false
     end
   end
